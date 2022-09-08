3d901172ea2d2cd8eb3dbff284e3356a84016ee0
dmitry <dmitrsafonov@gmail.com>
2022-09-08 17:26:21 +0300

    третье задание

--- ex_3.py
+++ ex_3.py
@@ -2,3 +2,12 @@
 # округлённую до трёх знаков после точки.
 # Пример:
 # Для n = 6 -> 14.072
+
+n = int(input('Введите число: ')) 
+
+def sequence(n):
+
+    return[round((1 + 1 / x)**x, 3) for x in range (1, n + 1)]   
+        
+print(sequence(n))
+print(round(sum(sequence(n)), 3))
